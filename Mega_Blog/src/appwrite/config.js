a64a5411a30f1de05aa09beeac237660010b7009
akashmaurya1372001-netizen <akashmaurya1372001@gmail.com>
2026-03-19 23:55:58 +0530

    getPost function

--- Mega_Blog/src/appwrite/config.js
+++ Mega_Blog/src/appwrite/config.js
@@ -1,67 +1,76 @@
-import conf from '../conf/conf.js';
+import confi from "../confi/confi.js";
 import { Client, ID, Databases, Storage, Query } from "appwrite";
 
-export class Service{
-    client = new Client();
-    databases;
-    bucket;
+export class Service {
+  client = new Client();
+  databases;
+  bucket;
 
-     constructor(){
-        this.client
-        .setEndpoint(conf.appwriteUrl)
-        .setProject(conf.appwriteProjectId);
-        this.databases = new Databases(this.client);
-        this.bucket = new Storage(this.client);
+  constructor() {
+    this.client
+      .setEndpoint(confi.appwriteURl)
+      .setProject(confi.appwriteProjectId);
+    this.databases = new Databases(this.client);
+    this.bucket = new Storage(this.client);
+  }
+  async createPost({ title, slug, content, featuredImage, status, userId }) {
+    try {
+      return await this.databases.createDocument(
+        confi.appwriteDatabaseId,
+        confi.appwriteCollectionId,
+        slug,
+        {
+          title,
+          content,
+          featuredImage,
+          status,
+          userId,
+        },
+      );
+    } catch (error) {
+      console.log("Appwrite serive :: createPost :: error", error);
     }
-    async createPost({title, slug, content, featuredImage, status, userId}){
-        try {
-            return await this.databases.createDocument(
-                conf.appwriteDatabaseId,
-                conf.appwriteCollectionId,
-                slug,
-                {
-                    title,
-                    content,
-                    featuredImage,
-                    status,
-                    userId,
-                }
-            )
-        } catch (error) {
-            console.log("Appwrite serive :: createPost :: error", error);
-        }
+  }
+  async updatePost(slug, { title, content, featuredImage, status }) {
+    try {
+      return await this.databases.updateDocument(
+        confi.appwriteDatabaseId,
+        confi.appwriteCollectionId,
+        slug,
+        {
+          title,
+          content,
+          featuredImage,
+          status,
+        },
+      );
+    } catch (error) {
+      console.log("Appwrite serive :: updatePost :: error", error);
     }
-    async updatePost(slug, {title, content, featuredImage, status}){
-        try {
-            return await this.databases.updateDocument(
-                conf.appwriteDatabaseId,
-                conf.appwriteCollectionId,
-                slug,
-                {
-                    title,
-                    content,
-                    featuredImage,
-                    status,
-
-                }
-            )
-        } catch (error) {
-            console.log("Appwrite serive :: updatePost :: error", error);
-        }
+  }
+  async deletePost(slug) {
+    try {
+      await this.databases.deleteDocument(
+        confi.appwriteDatabaseId,
+        confi.appwriteCollectionId,
+        slug,
+      );
+      return true;
+    } catch (error) {
+      console.log("Appwrite serive :: deletePost :: error", error);
+      return false;
     }
-    async deletePost(slug){
-        try {
-            await this.databases.deleteDocument(
-                conf.appwriteDatabaseId,
-                conf.appwriteCollectionId,
-                slug
-            
-            )
-            return true
-        } catch (error) {
-            console.log("Appwrite serive :: deletePost :: error", error);
-            return false
-        }
+  }
+  async getPost(slug) {
+    try {
+      return await this.databases.getDocument(
+        confi.appwriteDatabaseId,
+        confi.appwriteCollectionId,
+        slug,
+      );
+    } catch (error) {
+      console.log("Appwrite serive :: getPost :: error", error);
+      return false;
     }
-
+  }
 }
