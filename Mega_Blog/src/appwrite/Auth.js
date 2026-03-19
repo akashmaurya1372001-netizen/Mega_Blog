a26551ae4209ea0a592e55fedb9a51431d973c03
akashmaurya1372001-netizen <akashmaurya1372001@gmail.com>
2026-03-19 18:20:56 +0530

    creatAccount

--- Mega_Blog/src/appwrite/Auth.js
+++ Mega_Blog/src/appwrite/Auth.js
@@ -11,6 +11,20 @@ export class AuthService {
             .setProject(conf.appwriteProjectId);
         this.account = new Account(this.client);
             
+
+    }
+     async createAccount({email, password, name}) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if (userAccount) {
+                // call another method
+                return this.login({email, password});
+            } else {
+               return  userAccount;
+            }
+        } catch (error) {
+            throw error;
+        }
     }
 }
 
