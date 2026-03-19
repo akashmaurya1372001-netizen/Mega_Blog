1888915312f7b524428af1d280722fa6c779deae
akashmaurya1372001-netizen <akashmaurya1372001@gmail.com>
2026-03-19 18:13:21 +0530

    creaing Auth files

---
+++ Mega_Blog/src/appwrite/Auth.js
@@ -0,0 +1,18 @@
+import confi from "../confi.js";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client);
+            
+    }
+}
+
+const authService= new AuthService();
+export default authService;
