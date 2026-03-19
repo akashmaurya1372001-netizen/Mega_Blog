343977e1a4dd497272de06ad6dea835d1d4ab8ea
akashmaurya1372001-netizen <akashmaurya1372001@gmail.com>
2026-03-19 23:27:34 +0530

    deletePoste function

--- Mega_Blog/src/appwrite/config.js
+++ Mega_Blog/src/appwrite/config.js
@@ -31,4 +31,37 @@ export class Service{
             console.log("Appwrite serive :: createPost :: error", error);
         }
     }
+    async updatePost(slug, {title, content, featuredImage, status}){
+        try {
+            return await this.databases.updateDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+
+                }
+            )
+        } catch (error) {
+            console.log("Appwrite serive :: updatePost :: error", error);
+        }
+    }
+    async deletePost(slug){
+        try {
+            await this.databases.deleteDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug
+            
+            )
+            return true
+        } catch (error) {
+            console.log("Appwrite serive :: deletePost :: error", error);
+            return false
+        }
+    }
+
 }
