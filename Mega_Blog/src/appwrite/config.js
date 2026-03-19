2589247903abe37e683f37aa32be6ce0ddd73fd8
akashmaurya1372001-netizen <akashmaurya1372001@gmail.com>
2026-03-20 00:12:11 +0530

    deleteFile

--- Mega_Blog/src/appwrite/config.js
+++ Mega_Blog/src/appwrite/config.js
@@ -90,12 +90,24 @@ export class Service {
    async uploadFile(file){
         try {
             return await this.bucket.createFile(
-                conf.appwriteBucketId,
+                confi.appwriteBucketId,
                 ID.unique(),
                 file
             )
         } catch (error) {
             console.log("Appwrite serive :: uploadFile :: error", error);
+            return false
+        }
+    }
+    async deleteFile(fileId){
+        try {
+            await this.bucket.deleteFile(
+                conf.appwriteBucketId,
+                fileId
+            )
+            return true
+        } catch (error) {
+            console.log("Appwrite serive :: deleteFile :: error", error);
             return false
         }
     }
