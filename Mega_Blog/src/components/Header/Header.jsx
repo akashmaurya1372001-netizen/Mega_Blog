29b36959eedef3a4457a89abd7a18fd654b5dbcd
akashmaurya1372001-netizen <akashmaurya1372001@gmail.com>
2026-03-21 10:16:56 +0530

    fixing error

--- Mega_Blog/src/components/Header/Header.jsx
+++ Mega_Blog/src/components/Header/Header.jsx
@@ -3,24 +3,41 @@ import { useNavigate } from "react-router-dom";
 import {container,Logo,LogoutBtn} from  '../index'
 import {useNavigate} from 'react-router-dom'
 import { Link } from 'react-router-dom'
-
+import { useSelector } from "react-redux";
+import {useSelector} from 'rect-redux'
 
 function Header(){
-    const authService= useSelector((state)=>satisfies.auth.status)
-    const navigate=useNavigate()
+    const authStatus= useSelector((state)=> state.auth.status)
+    const navigate = useNavigate()
 
-    const navItems=[
-        {
-            name:'home'
-            slug:"/",
-            active:true 
-        },
-        {
-          name:'login'
-            slug:"/login",
-            active:!authStatus, 
-        }, 
-    ]
+    const navItems = [
+    {
+      name: 'Home',
+      slug: "/",
+      active: true
+    }, 
+    {
+      name: "Login",
+      slug: "/login",
+      active: !authStatus,
+  },
+  {
+      name: "Signup",
+      slug: "/signup",
+      active: !authStatus,
+  },
+  {
+      name: "All Posts",
+      slug: "/all-posts",
+      active: authStatus,
+  },
+  {
+      name: "Add Post",
+      slug: "/add-post",
+      active: authStatus,
+  },
+  ]
+
         }
     
     return
