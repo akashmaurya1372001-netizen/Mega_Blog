6fd06d6a15b56c276d52492064e28322bc12cb91
akashmaurya1372001-netizen <akashmaurya1372001@gmail.com>
2026-03-22 00:06:29 +0530

    working in postForm

--- Mega_Blog/src/components/PostForm/postForm.jsx
+++ Mega_Blog/src/components/PostForm/postForm.jsx
@@ -4,3 +4,13 @@ import { Button, Input, RTE, Select } from "..";
 import appwriteService from "../../appwrite/config";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+
+export default function PostForm({ post }) {
+    const { register, handleSubmit, watch, setValue, control, getValues } = useForm({
+        defaultValues: {
+            title: post?.title || "",
+            slug: post?.$id || "",
+            content: post?.content || "",
+            status: post?.status || "active",
+        },
+    })}
